fe74f0a29fd889d636d3cb22d42be864426f3720
Pariya <pariyaafsharpour79@gmail.com>
2025-08-03 19:48:56 +0330

    Implemented train_model using KNeighborsClassifier with k=1

--- code/shopping/shopping/shopping.py
+++ code/shopping/shopping/shopping.py
@@ -96,8 +96,10 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
-
+    model = KNeighborsClassifier(1)
+    model.fit(evidence, labels)
+    return model
+    # raise NotImplementedError
 
 def evaluate(labels, predictions):
     """
