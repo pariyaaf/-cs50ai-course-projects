36ea4cea0e1a65a5fdcea2dd616338b7b0018723
Pariya <pariyaafsharpour79@gmail.com>
2025-08-04 15:23:40 +0330

    Implement get_q_value to retrieve Q for a state-action pair

--- code/nim/nim/nim.py
+++ code/nim/nim/nim.py
@@ -101,7 +101,8 @@ class NimAI():
         Return the Q-value for the state `state` and the action `action`.
         If no Q-value exists yet in `self.q`, return 0.
         """
-        raise NotImplementedError
+        return self.q.get((tuple(state), action), 0)
+        # raise NotImplementedError
 
     def update_q_value(self, state, action, old_q, reward, future_rewards):
         """
